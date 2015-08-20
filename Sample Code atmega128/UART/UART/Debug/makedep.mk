7ae4a28bca9e63c5e6dce0a22f89cfb3c615c4ca
Raghavendra <raghavendra@exploreembedded.com>
2015-08-20 12:54:35 +0530

    UART code added

--- Sample Code atmega128/UART/UART/Debug/makedep.mk
+++ Sample Code atmega128/UART/UART/Debug/makedep.mk
@@ -2,5 +2,7 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-SerialCommunication.c
+main.c
+
+UART.c
 
